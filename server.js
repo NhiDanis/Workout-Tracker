422a2cceffbdaa4723f8ab844039e281bc38dd58
Nhi Danis <a.nhi1001@gmail.com>
2021-02-07 15:21:46 -0800

    commit set up routes

--- server.js
+++ server.js
@@ -23,3 +23,19 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
+
+app.use(require("./routes/api-routes.js"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+
